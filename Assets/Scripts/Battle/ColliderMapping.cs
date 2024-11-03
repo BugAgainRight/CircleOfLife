0070697f835e0dfffc5081abbc7450bff56c5a67
222200414陈志琰 <ris_vb@126.com>
2024-11-03 22:09:44 +0800

    feat: 碰撞箱映射器、玩家封装

---
+++ Assets/Scripts/Battle/ColliderMapping.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CircleOfLife.Battle
+{
+    public static class ColliderMapping
+    {
+        private static readonly Dictionary<Collider2D, BattleStats> map = new();
+
+        public static void RegisterCollider(Collider2D collider, BattleStats stats)
+        {
+            map.TryAdd(collider, stats);
+        }
+
+        public static void RevokeCollider(Collider2D collider)
+        {
+            map.Remove(collider);
+        }
+
+        public static BattleStats GetBattleStats(this Collider2D collider)
+        {
+            return map[collider];
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/PlayerUtils.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace CircleOfLife.Utils
+{
+    public static class PlayerUtils
+    {
+        public static bool IsPlayer(this GameObject go)
+            => go.CompareTag("Player");
+    }
+}
