ca434882505c62301e17ffc789099df627fbffb1
222200233 <1205141035@qq.com>
2024-11-10 01:29:21 +0800

    chore:键盘设置测试代码

--- Assets/EditTests/TestDDialogManager.cs
+++ Assets/EditTests/TestDDialogManager.cs
@@ -8,62 +8,66 @@ using TMPro;
 using UnityEngine.UI;
 using System;
 using System.Reflection;
-public class DialogManagerTests
+
+namespace CircleOfLife.EditModeTest
 {
-    [UnityTest]
-    public IEnumerator ReadDialogDataSuccess()
+    public class DialogManagerTests
     {
-        // 创建一个包含 DialogManager 的 GameObject
-        GameObject dialogManagerObject = new GameObject("DialogManagerObject");
-        DialogManager dialogManager = dialogManagerObject.AddComponent<DialogManager>();
+        [UnityTest]
+        public IEnumerator ReadDialogDataSuccess()
+        {
+            // 创建一个包含 DialogManager 的 GameObject
+            GameObject dialogManagerObject = new GameObject("DialogManagerObject");
+            DialogManager dialogManager = dialogManagerObject.AddComponent<DialogManager>();
 
-        TextAsset dialogDataFile = Resources.Load<TextAsset>("Texts/test");
+            TextAsset dialogDataFile = Resources.Load<TextAsset>("Texts/test");
 
-        dialogManager.DialogDataFile = dialogDataFile;
-        dialogManager.ReadText(dialogManager.DialogDataFile);
+            dialogManager.DialogDataFile = dialogDataFile;
+            dialogManager.ReadText(dialogManager.DialogDataFile);
 
-        GameObject.DestroyImmediate(dialogManagerObject);
+            GameObject.DestroyImmediate(dialogManagerObject);
 
-        yield return null;
-    }
-    [UnityTest]
-    public IEnumerator DialogUpdateSuccess()
-    {
-        // 创建一个包含 DialogManager 的 GameObject
-        GameObject dialogManagerObject = new GameObject("DialogManagerObject");
-        DialogManager dialogManager = dialogManagerObject.AddComponent<DialogManager>();
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator DialogUpdateSuccess()
+        {
+            // 创建一个包含 DialogManager 的 GameObject
+            GameObject dialogManagerObject = new GameObject("DialogManagerObject");
+            DialogManager dialogManager = dialogManagerObject.AddComponent<DialogManager>();
 
-        TextAsset dialogDataFile = Resources.Load<TextAsset>("Texts/test");
-        dialogManager.ReadText(dialogDataFile);
+            TextAsset dialogDataFile = Resources.Load<TextAsset>("Texts/test");
+            dialogManager.ReadText(dialogDataFile);
 
-        GameObject NameTextObject = new GameObject("NameTextObject");
-        GameObject ContentTextObject = new GameObject("ContentTextObject");
+            GameObject NameTextObject = new GameObject("NameTextObject");
+            GameObject ContentTextObject = new GameObject("ContentTextObject");
 
-        dialogManager.NameText = NameTextObject.AddComponent<TextMeshProUGUI>();
-        dialogManager.ContentText = ContentTextObject.AddComponent<TextMeshProUGUI>();
+            dialogManager.NameText = NameTextObject.AddComponent<TextMeshProUGUI>();
+            dialogManager.ContentText = ContentTextObject.AddComponent<TextMeshProUGUI>();
 
-        dialogManager.UpdateText("test1", "test2");
-        
-        Assert.AreEqual("test1", dialogManager.NameText.text);
-        Assert.AreEqual("test2", dialogManager.ContentText.text);
+            dialogManager.UpdateText("test1", "test2");
+
+            Assert.AreEqual("test1", dialogManager.NameText.text);
+            Assert.AreEqual("test2", dialogManager.ContentText.text);
 
 
-        GameObject.DestroyImmediate(dialogManagerObject);// 清理
-        yield return null;
-    }
-    [UnityTest]
-    public IEnumerator ButtonCreateCorrect()
-    {
-        // 创建一个包含 DialogManager 的 GameObject
-        GameObject dialogManagerObject = new GameObject("DialogManagerObject");
-        DialogManager dialogManager = dialogManagerObject.AddComponent<DialogManager>();
+            GameObject.DestroyImmediate(dialogManagerObject);// 清理
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator ButtonCreateCorrect()
+        {
+            // 创建一个包含 DialogManager 的 GameObject
+            GameObject dialogManagerObject = new GameObject("DialogManagerObject");
+            DialogManager dialogManager = dialogManagerObject.AddComponent<DialogManager>();
 
-        TextAsset dialogDataFile = Resources.Load<TextAsset>("Texts/test");
-        dialogManager.ReadText(dialogDataFile);
+            TextAsset dialogDataFile = Resources.Load<TextAsset>("Texts/test");
+            dialogManager.ReadText(dialogDataFile);
 
-        dialogManager.GenerateOption(1);
+            dialogManager.GenerateOption(1);
 
-        GameObject.DestroyImmediate(dialogManagerObject);// 清理
-        yield return null;
+            GameObject.DestroyImmediate(dialogManagerObject);// 清理
+            yield return null;
+        }
     }
 }
---
+++ Assets/EditTests/TestKeyboardSet.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework; // 使用 NUnit 测试框架
+using UnityEngine; // 使用 UnityEngine 命名空间
+using UnityEngine.TestTools; // 使用 Unity Test Framework
+using CircleOfLife.Key;
+using System.Collections; // 使用 CircleOfLife.Key 命名空间
+public class KeyboardSetTests
+{
+    [UnityTest]
+    public IEnumerator TestKeyboardMapping()
+    {
+        // 测试键盘映射是否正确
+        Assert.AreEqual(KeyCode.W, KeyboardSet.GetKeyCode(KeyEnum.Up));
+        Assert.AreEqual(KeyCode.S, KeyboardSet.GetKeyCode(KeyEnum.Down));
+        Assert.AreEqual(KeyCode.A, KeyboardSet.GetKeyCode(KeyEnum.Left));
+        Assert.AreEqual(KeyCode.D, KeyboardSet.GetKeyCode(KeyEnum.Right));
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator TestChangeKey()
+    {
+        // 测试更改键位
+        KeyboardSet.ChangeKey(KeyEnum.Up, KeyCode.L);
+        Assert.AreEqual(KeyCode.L, KeyboardSet.GetKeyCode(KeyEnum.Up));
+        yield return null;
+        // 测试重复键位是否被阻止
+        KeyboardSet.ChangeKey(KeyEnum.Up, KeyCode.D);
+        Assert.AreNotEqual(KeyCode.D, KeyboardSet.GetKeyCode(KeyEnum.Up));
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator TestResetKey()
+    {
+        // 测试重置键位
+        KeyboardSet.ResetKey(KeyEnum.Up);
+        Assert.AreEqual(KeyCode.None, KeyboardSet.GetKeyCode(KeyEnum.Up));
+
+        yield return null;
+    }
+}
+
