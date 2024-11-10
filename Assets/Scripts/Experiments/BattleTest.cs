367f7a284d20de8dacc61fefc718c4aa0243a0af
222200414陈志琰 <ris_vb@126.com>
2024-11-10 14:45:48 +0800

    chore: 天气系统效果微调

--- Assets/Scripts/Experiments/BattleTest.cs
+++ Assets/Scripts/Experiments/BattleTest.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using CircleOfLife.Build;
+using CircleOfLife.Weather;
 using UnityEngine;
 
 namespace CircleOfLife.Experiments
 {
     public class BattleTest : MonoBehaviour
     {
+        public WeatherSystem.Weather Weather;
         private void Start()
         {
+            WeatherSystem.CurrentWeather = Weather;
             BuildUtils.EnableAllBuilding();
         }
     }
