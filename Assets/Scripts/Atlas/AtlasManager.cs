6f9a5b20a9a44fa5a3a6b889988621f1f3f03f61
222200414陈志琰 <ris_vb@126.com>
2024-11-16 23:29:28 +0800

    feat: 图鉴SO和读取

---
+++ Assets/Scripts/Atlas/AtlasManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CircleOfLife.Atlas
+{
+    public class AtlasManager : MonoBehaviour
+    {
+        public static List<AtlasData> Data { get; private set; } = new();
+
+        [RuntimeInitializeOnLoadMethod]
+        public static void Initialize()
+        {
+            Data = Resources.Load<AtlasSO>("Atlas/AtlasData").Data;
+        }
+    }
+}
---
+++ Assets/Scripts/Atlas/AtlasSO.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using Spine;
+using Spine.Unity;
+using UnityEngine;
+
+namespace CircleOfLife.Atlas
+{
+    [CreateAssetMenu]
+    public class AtlasSO : ScriptableObject
+    {
+        public List<AtlasData> Data = new();
+    }
+    
+    [Serializable]
+    public class AtlasData
+    {
+        public string Title;
+        [Multiline]
+        public string Description;
+        public Sprite Icon;
+        public SkeletonDataAsset SkeletonData;
+    }
+}
