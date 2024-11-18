7c0e68efcb105781d4edf840dc8349fc1508fa6e
nigulasirui <2573459273@qq.com>
2024-11-18 16:53:02 +0800

    feat：存档管理代码

---
+++ Assets/Scripts/Save/SaveManagement.cs
@@ -0,0 +1,124 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace CircleOfLife
+{
+    public class SaveManagement : MonoBehaviour
+    {
+        public static SaveManagement Instance;
+        private static SaveCombine saveData;
+        public static SaveCombine.SaveData UseSaveData;
+
+        [RuntimeInitializeOnLoadMethod]
+        private static void Init()
+        {
+            Debug.Log("存档位置:" + Path.Combine(Application.persistentDataPath, "SaveData.json"));
+            if (!File.Exists(Path.Combine(Application.persistentDataPath, "SaveData.json")))
+            {
+                saveData = new SaveCombine();
+                saveData.AllSaveData = new() { null,null,null,null};
+
+                File.WriteAllText(Path.Combine(Application.persistentDataPath, "SaveData.json"),
+                    JsonConvert.SerializeObject(saveData));
+            }
+            saveData = JsonConvert.DeserializeObject<SaveCombine>(
+                File.ReadAllText(Path.Combine(Application.persistentDataPath, "SaveData.json")));
+          
+        }
+
+        /// <summary>
+        /// 保存saveData数据
+        /// </summary>
+        public static void Save(int index)
+        {
+            if (index < 4) saveData.AllSaveData[index] = UseSaveData;
+            File.WriteAllText(Path.Combine(Application.persistentDataPath, "SaveData.json"),
+                JsonConvert.SerializeObject(saveData));
+            UseSaveData = null;
+        }
+
+        /// <summary>
+        /// 选择存档，
+        /// </summary>
+        /// <param name="index"></param>
+        public static void SelectSaveData(int index)
+        {
+            if (index < 0)
+            {
+                UseSaveData = new SaveCombine.SaveData();
+            }
+            else if (index < 4)
+            {
+                UseSaveData = new SaveCombine.SaveData();
+                if (saveData.AllSaveData[index] != null) UseSaveData.Copy(saveData.AllSaveData[index]);
+            }
+        }
+
+
+        /// <summary>
+        /// 获取所有存档信息
+        /// </summary>
+        /// <returns></returns>
+        public static List<SaveCombine.SaveData> GetAllSaveData()
+        {
+            return saveData.AllSaveData;
+        }
+
+        private void FixedUpdate()
+        {
+
+            if (UseSaveData!=null)
+            {
+                UseSaveData.AddTimer();
+            }
+
+        }
+
+    }
+
+    [SerializeField]
+    public class SaveCombine
+    {
+        [SerializeField]
+        public class SaveData
+        {
+            [JsonProperty]
+            private long timer;
+            [JsonProperty]
+            private float midTimer;
+            [JsonIgnore]
+            public TimeSpan Timer=>TimeSpan.FromSeconds(timer);
+
+
+            ////////////其他存档数据
+
+
+
+
+            ////////////
+            
+
+            public void AddTimer()
+            {
+                midTimer += Time.fixedDeltaTime;
+                if (midTimer >= 1)
+                {
+                    timer += (int)midTimer;
+                    midTimer -= (int)midTimer;
+                }
+            }
+            public void Copy(SaveData other)
+            {
+                timer=other.timer;
+                midTimer = other.midTimer;
+            }
+
+        }
+        public List<SaveData> AllSaveData = new();
+
+    }
+}
